9e1eed457114828ef63141a424b04d17d044059c
saboumariusbogdan <saboumariusbogdan.home@gmail.com>
2021-05-11 18:14:40 +0300

    few methods in userRepository

--- src/main/java/ro/sda/java15remote/finalProject/database/repository/UserRepository.java
+++ src/main/java/ro/sda/java15remote/finalProject/database/repository/UserRepository.java
@@ -4,8 +4,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ro.sda.java15remote.finalProject.database.entities.User;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
+    List<User> findUsersByFirstNameAndLastName(String firstName, String lastName);
+    List<User> findUsersByLastName(String lastName);
+    List<User> findUsersByFirstName(String firstName);
+    List<User> findUserByTotalNumberOfPoints(Integer totalNumberOfPoints);
+
+
+
+
 }
